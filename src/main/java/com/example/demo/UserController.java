424183e96d3b94c7a6f19c3a6691d9a8ee9f78a2
Bannavith Khuhaprema <bannavithkhu@mathregit.group>
2020-08-05 11:49:06 +0700

    step02

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,15 +1,25 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
     @GetMapping("/users")
-    public UserResponse[] getAllUsers(){
-        UserResponse[] userResponses = new UserResponse[2];
-        userResponses[0] = new UserResponse(1,"user1");
-        userResponses[1] = new UserResponse(2,"user2");
+    public List<UserResponse> getAllUsers(@RequestParam(required = false) int page, int itemPerPage){
+        List<UserResponse> userResponses = new ArrayList<>();
+        userResponses.add(new UserResponse(1,"user1"));
+        userResponses.add(new UserResponse(2,"user2"));
         return userResponses;
     }
+    @GetMapping("/users/{id}")
+    public UserResponse getUserById(@PathVariable int id){
+        return new UserResponse(id, "User " + id);
+    }
+
 }
